962b8192a5198ce2e314d0dc5d0c16aac3bfcc21
Mcriado <mcriado.cg@gmail.com>
2020-04-18 01:29:56 -0400

    Finalizada sección quick-access y scripts

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const colorRedO = document.getElementById('o-red');
+const colorRedE = document.getElementById('e-red');
+const colorYellow = document.getElementById('yellow');
+const colorGreen = document.getElementById('green');
+
+colorRedO.style.color = '#EB4134'
+colorRedE.style.color = '#EB4134'
+colorYellow.style.color = '#FBBA04'
+colorGreen.style.color = '#34AA51'
